41f12b7853bb74fcef8576639595e40940c52c85
BryanGerman <bryanglema2@gmail.com>
2018-02-19 21:36:57 -0500

    Creacion de carpetas
    Creacion de carpetas con los respectivos archivos ejecutables y resultados.

---
+++ Filtrado/scriptFiltrado.sh
@@ -0,0 +1,17 @@
+cat salida.json | grep "consultapopular" >> resultadoFinal.json
+cat salida.json | grep "Mashi" >> resultadoFinal.json
+cat salida.json | grep "mashi" >> resultadoFinal.json
+cat salida.json | grep "@Lenin" >> resultadoFinal.json
+cat salida.json | grep "sieteveces" >> resultadoFinal.json
+cat salida.json | grep -w "SI" | grep "pueblo" >> resultadoFinal.json
+cat salida.json | grep -w "NO" | grep "pueblo" >> resultadoFinal.json
+cat salida.json | grep -w "Ratael" >> resultadoFinal.json
+cat salida.json | grep -w "gana" | grep "SI" >> resultadoFinal.json
+cat salida.json | grep -w "Ecuador" | grep -w "SI" | grep -w "gana"  >> resultadoFinal.json
+cat salida.json | grep -w "EcuadorDiceSi" >> resultadoFinal.json
+cat salida.json | grep -w "7VecesSi" >> resultadoFinal.json
+cat salida.json | grep -w "7VecesNo" >> resultadoFinal.json
+cat salida.json | grep -w "correa" | grep "consulta" >> resultadoFinal.json
+
+
+
